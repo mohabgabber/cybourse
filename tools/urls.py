1e8e834596a8dacda9d89d3ec409e676378db98f
Mohab Gabber <mohabgabber0@gmail.com>
2024-07-11 13:09:46 +0300

    removed ipa keyboard

--- tools/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path("ipa/keyboard/", IPAKeyboard.as_view(), name="ipa-keyboard"),
-]
--- tools/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views import View
-
-
-class IPAKeyboard(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "tools/ipa_keyboard.html")
